fcdf3283972c412d1af90d17f01b01a2956f892d
bb420f@gmail.com <bb420f@gmail.com>
2023-05-16 11:17:00 +0500

    5th page done

--- js/script.js
+++ js/script.js
@@ -3,54 +3,38 @@ $(document).ready(function () {
     $(".nav_links").slideToggle();
     $(this).find('.ti').toggleClass("ti-x");
   });
+  $(".ti-caret-down").click(function () {
+    $(".cate_bottom").slideToggle();
+    $(this).toggleClass("ti-caret-down").toggleClass('ti-caret-up')
+  })
+});
 
-  var swiper = new Swiper(".cate_silde_main", {
-    loop: true,
-    slidesPerView: "auto",
-   centeredSlides: true,
-    speed: 500,
-    spaceBetween: 10,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
-    // breakpoints: {
-    //   640: {
-    //     slidesPerView: 2,
-    //     spaceBetween: 20
-    //   },
-    //   768: {
-    //     slidesPerView: 4,
-    //     spaceBetween: 30
-    //   },
-    //   1024: {
-    //     slidesPerView: 6,
-    //     spaceBetween: 30
-    //   }
-  
-    // }
-   
-  });
+var swiper = new Swiper(".cate_silde_main", {
+  loop: true,
+  slidesPerView: "auto",
+  centeredSlides: true,
+  speed: 500,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
 
 
-
-
-  var swiper = new Swiper(".store_swiper", {
-    slidesPerView: "auto",
-    grid: {
-      rows: 2,
-    },
-    spaceBetween: 30,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+var swiper = new Swiper(".store_swiper", {
+  slidesPerView: "auto",
+  grid: {
+    rows: 2,
+  },
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
   // $(document).ready(function() {
   //   $("input").click(function() {
   //     $(this).css("border", "2px solid red");
   //   });
   // });
-  
-});
-
